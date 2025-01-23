a8664a2efd345641b8ecf252ee8b18e3fd5cbed0
ChoHyunWoo <gusdndlek12@naver.com>
2025-01-23 15:32:17 +0900

    timeplate5

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -1,43 +1,30 @@
 package com.example.template.domain.post.post.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
+@Validated
 public class PostController {
 
     @GetMapping("/write")
     @ResponseBody
     public String showWrite() {
-        return """
-                <form method="post">
-                  <input type="text" name="title" placeholder="제목" />
-                  <textarea name="content"></textarea>
-                  <input type="submit" value="등록" />
-                </form>
-                """;
+        return getFormHtml("");
     }
 
     @PostMapping("/write")
     @ResponseBody
-    public String doWrite(String title, String content) {
+    public String doWrite(
+            @NotBlank @Length(min=5) String title,
+            @NotBlank @Length(min=10) String content
+    ) {
 
-        if (title.isBlank() || title == null) {
-            return getFormHtml("제목을 입력해주세요.");
-        }
-
-        if (content.isBlank() || content == null) {
-            return getFormHtml("내용을 입력해주세요.");
-        }
-
-        if (title.length() < 5) {
-            return getFormHtml("제목은 5글자 이상");
-        }
-
-        if (content.length() < 10) {
-            return getFormHtml("내용은 10글자 이상.");
-        }
         return """
                 <h1>게시물 조회</h1>
                 <div>%s</div>
