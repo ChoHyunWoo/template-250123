dc04f2b98544ae74b29b26a135d610a9e7e83fb1
ChoHyunWoo <gusdndlek12@naver.com>
2025-01-23 15:13:21 +0900

    timeplate2

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -1,13 +1,75 @@
 package com.example.template.domain.post.post.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
+@RequestMapping("/posts")
 public class PostController {
 
-    @GetMapping
+    @GetMapping("/write")
     @ResponseBody
-    public String hello(){
-        return "hello";
+    public String showWrite() {
+        return """
+                <form method = "post">
+                  <input type="text" name="title" placeholder="제목" />
+                  <textarea name="content"></textarea>
+                  <input type="submit" value="등록" />
+                </form>
+                """;
+    }
+
+    // 브라우저 주소창에 주소 치는 것 ->get
+    //form이 보내주는 주소 -> get
+
+    @PostMapping("/write")
+    @ResponseBody
+    public String doWrite(String title, String content) {
+
+        if(title.isBlank() || title == null){
+            return """
+                <div>%s</div>
+                <form method = "post">
+                  <input type="text" name="title" placeholder="제목" />
+                  <textarea name="content"></textarea>
+                  <input type="submit" value="등록" />
+                </form>
+                """.formatted("제목을 입력해주세요.");
+        }
+        if(content.isBlank() || content == null){
+            return """
+                <div>%s</div>
+                <form method = "post">
+                  <input type="text" name="title" placeholder="제목" />
+                  <textarea name="content"></textarea>
+                  <input type="submit" value="등록" />
+                </form>
+                """.formatted("내용을 입력해주세요.");
+        }
+        if(title.length() <5){
+            return """
+                <div>%s</div>
+                <form method = "post">
+                  <input type="text" name="title" placeholder="제목" />
+                  <textarea name="content"></textarea>
+                  <input type="submit" value="등록" />
+                </form>
+                """.formatted("제목 5글자이상.");
+        if(content.length() <10){
+            return """
+                <div>%s</div>
+                <form method = "post">
+                  <input type="text" name="title" placeholder="제목" />
+                  <textarea name="content"></textarea>
+                  <input type="submit" value="등록" />
+                </form>
+                """.formatted("내용 10글자 이상");
+        }
+
+        return """
+                <h1>게시물 조회</h1>
+                <div>%s</div>
+                <div>%s</div>
+                """.formatted(title, content);
     }
 }
