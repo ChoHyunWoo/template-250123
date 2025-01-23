af5ba9e52a782a009874626dbd1d68f17c17bc13
ChoHyunWoo <gusdndlek12@naver.com>
2025-01-23 15:15:09 +0900

    timeplate3

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -11,7 +11,7 @@ public class PostController {
     @ResponseBody
     public String showWrite() {
         return """
-                <form method = "post">
+                <form method="post">
                   <input type="text" name="title" placeholder="제목" />
                   <textarea name="content"></textarea>
                   <input type="submit" value="등록" />
@@ -19,57 +19,57 @@ public class PostController {
                 """;
     }
 
-    // 브라우저 주소창에 주소 치는 것 ->get
-    //form이 보내주는 주소 -> get
-
     @PostMapping("/write")
     @ResponseBody
     public String doWrite(String title, String content) {
 
-        if(title.isBlank() || title == null){
+        if (title.isBlank() || title == null) {
             return """
-                <div>%s</div>
-                <form method = "post">
-                  <input type="text" name="title" placeholder="제목" />
-                  <textarea name="content"></textarea>
-                  <input type="submit" value="등록" />
-                </form>
-                """.formatted("제목을 입력해주세요.");
-        }
-        if(content.isBlank() || content == null){
-            return """
-                <div>%s</div>
-                <form method = "post">
-                  <input type="text" name="title" placeholder="제목" />
-                  <textarea name="content"></textarea>
-                  <input type="submit" value="등록" />
-                </form>
-                """.formatted("내용을 입력해주세요.");
-        }
-        if(title.length() <5){
-            return """
-                <div>%s</div>
-                <form method = "post">
-                  <input type="text" name="title" placeholder="제목" />
-                  <textarea name="content"></textarea>
-                  <input type="submit" value="등록" />
-                </form>
-                """.formatted("제목 5글자이상.");
-        if(content.length() <10){
-            return """
-                <div>%s</div>
-                <form method = "post">
-                  <input type="text" name="title" placeholder="제목" />
-                  <textarea name="content"></textarea>
-                  <input type="submit" value="등록" />
-                </form>
-                """.formatted("내용 10글자 이상");
+                    <div>%s</div>
+                    <form method="post">
+                      <input type="text" name="title" placeholder="제목" />
+                      <textarea name="content"></textarea>
+                      <input type="submit" value="등록" />
+                    </form>
+                    """.formatted("제목을 입력해주세요.");
         }
 
+        if (content.isBlank() || content == null) {
+            return """
+                    <div>%s</div>
+                    <form method="post">
+                      <input type="text" name="title" placeholder="제목" />
+                      <textarea name="content"></textarea>
+                      <input type="submit" value="등록" />
+                    </form>
+                    """.formatted("내용을 입력해주세요.");
+        }
+
+        if (title.length() < 5) {
+            return """
+                    <div>%s</div>
+                    <form method="post">
+                      <input type="text" name="title" placeholder="제목" />
+                      <textarea name="content"></textarea>
+                      <input type="submit" value="등록" />
+                    </form>
+                    """.formatted("제목은 5글자 이상");
+        }
+        if (content.length() < 10) {
+            return """
+                    <div>%s</div>
+                    <form method="post">
+                      <input type="text" name="title" placeholder="제목" />
+                      <textarea name="content"></textarea>
+                      <input type="submit" value="등록" />
+                    </form>
+                    """.formatted("내용은 10글자 이상.");
+        }
         return """
                 <h1>게시물 조회</h1>
                 <div>%s</div>
                 <div>%s</div>
                 """.formatted(title, content);
     }
+
 }
