366b7d1d27a20fc348d451b63f006ed9bfe4758b
ChoHyunWoo <gusdndlek12@naver.com>
2025-01-24 09:33:11 +0900

    msg

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -1,17 +1,20 @@
 package com.example.template.domain.post.post.controller;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.*;
 
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/posts")
-@Validated
 public class PostController {
 
     @GetMapping("/write")
@@ -23,24 +26,33 @@ public class PostController {
     @AllArgsConstructor
     @Getter
     public static class WriteForm {
-        @NotBlank(message = "제목을 입력해주세요.") @Length(min = 5, message = "제목은 5글자 이상입니다.")
+        @NotBlank(message = "01-제목을 입력해주세요.")
+        @Length(min = 5, message = "02-제목은 5글자 이상입니다.")
         private String title;
-        @NotBlank(message = "내용을 입력해주세요.") @Length(min = 10, message = "내용은 10글자 이상입니다.")
+        @NotBlank(message = "03-내용을 입력해주세요.")
+        @Length(min = 10, message = "04-내용은 10글자 이상입니다.")
         private String content;
     }
 
     @PostMapping("/write")
     @ResponseBody
-    public String doWrite(@ModelAttribute @Validated WriteForm form) {
-
+    public String doWrite(@Valid WriteForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(err -> err.getDefaultMessage())
+                    .sorted()
+                    .map(msg -> msg.split("-")[1])
+                    .collect(Collectors.joining("<br>"));
+            return getFormHtml(errorMessage);
+        }
 
         return """
                 <h1>게시물 조회</h1>
                 <div>%s</div>
                 <div>%s</div>
-                """.formatted(form.title, form.content);
+                """.formatted(form.getTitle(), form.getContent());
     }
-
 
     private String getFormHtml(String errorMsg) {
         return """
@@ -52,6 +64,4 @@ public class PostController {
                 </form>
                 """.formatted(errorMsg);
     }
-
-
 }
